cab85e81fdd79f21e2800d70e697aa5bf3c5320c
“stanhu” <3421902@qq.com>
2017-11-01 17:52:09 +0800

    c first stage

---
+++ capter1.c
@@ -0,0 +1,13 @@
+
+#include <stdio.h>
+
+int main(){
+    int num;
+    num = 1;
+    printf("this is a simple");
+    printf("computer.\n");
+
+    printf("My favorite number is %d because it is first .\n",num);
+    return 1;
+}
+
