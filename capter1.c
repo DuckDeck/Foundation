2373f3a0c74e4d72bcadcf3b5ce8742d9e821ec5
“stanhu” <3421902@qq.com>
2017-11-03 14:44:16 +0800

    zz

--- capter1.c
+++ capter1.c
@@ -2,11 +2,11 @@
 #include <stdio.h>
 
 int main(){
-    int num;
+    short num;
     num = 1;
     printf("this is a simple");
     printf("computer.\n");
-
+    printf("%d \n",sizeof(num));
     printf("My favorite number is %d because it is first .\n",num);
     return 1;
 }
