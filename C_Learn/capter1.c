874b3fe9440171379773f469cbe2de3fdd13724e
“stanhu” <3421902@qq.com>
2017-11-21 08:58:58 +0800

    fix some thing

--- C_Learn/capter1.c
+++ C_Learn/capter1.c
@@ -6,7 +6,7 @@ int main(){
     num = 1;
     printf("this is a simple");
     printf("computer.\n");
-    printf("%d \n",sizeof(num));
+    printf("%lu \n",sizeof(num));
     printf("My favorite number is %d because it is first .\n",num);
     return 1;
 }
