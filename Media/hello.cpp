c5f6240dc232177801e874ecdeb961d78d0b7b95
Stan Hu <3421902@qq.com>
2018-08-04 19:13:45 +0800

    process code

--- Media/hello.cpp
+++ Media/hello.cpp
@@ -5,10 +5,13 @@
 #include "media.h"
 using namespace std;
 int yuv420_split(char* url,int w,int h,int num);
-
+int yuv444_split(char *url, int w, int h, int num);
+int yuv420_gray(char *url, int w, int h, int num);
 int main() {
 
-    yuv420_split("Media/material/yuv420.yuv",256,256,1);
+    //yuv420_split("Media/material/yuv420.yuv",256,256,1);
+    //yuv444_split("Media/material/yuv444p.yuv",256,256,1);
+    yuv420_gray("Media/material/yuv420.yuv",256,256,1);
     getchar();
     return 0;
 }
--- Media/media.h
+++ Media/media.h
@@ -32,7 +32,10 @@ int yuv420_split(char* url,int w,int h,int num){
     fclose(fp3);
     return 0;
 }
-
+//(2)分离YUV444P像素数据中的Y、U、V分量
+//从代码可以看出，如果视频帧的宽和高分别为w和h，那么一帧YUV444P像素数据一共占用w*h*3 Byte的数据。其中前w*h Byte存储Y，
+//接着的w*h Byte存储U，最后w*h Byte存储V。上述调用函数的代码运行后，将会把一张分辨率为256x256的名称为lena_256x256_yuv444p.
+//yuv的YUV444P格式的像素数据文件分离成为三个文件：
 int yuv444_split(char *url,int w,int h,int num){
     FILE *fp = fopen(url,"rb+");
     FILE *fp1 = fopen("Media/material/output_444_y.y","wb+");
@@ -53,6 +56,20 @@ int yuv444_split(char *url,int w,int h,int num){
     return 0;
 }
 
+//(3) 将YUV420P像素数据去掉颜色（变成灰度图）
 
-
+int yuv420_gray(char *url,int w,int h,int num){
+    FILE *fp = fopen(url, "rb+");
+    FILE *fp1 = fopen("Media/material/output_yuv420_gray.yuv", "wb+");
+    unsigned char *pic = (unsigned char *)malloc(w * h * 3 / 2);
+    for (int i = 0; i < num; i++){
+        fread(pic, 1, w * h * 3 / 2, fp);
+        memset(pic + w * h, 128, w * h / 2);
+        fwrite(pic, 1, w * h * 3 / 2, fp1);
+    }
+    free(pic);
+    fclose(fp);
+    fclose(fp1);
+    return 0;
+}
 #endif
