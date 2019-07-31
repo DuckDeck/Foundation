b81b3cddf44bb9453cec4bcedb98b50c6443aa9d
“stanhu” <3421902@qq.com>
2019-07-31 14:09:48 +0800

    process code

--- Compile/a.c
+++ Compile/a.c
@@ -42,3 +42,34 @@ Idx Name          Size      VMA               LMA               File off  Algn
   2 .eh_frame     00000040  0000000000000048  0000000000000048  00000220  2**3
                   CONTENTS, ALLOC, LOAD, READONLY, DATA
  */
+//可以看出这里和书本上很多不一样
+
+// objdump -h b.o
+/*
+b.o:     file format mach-o-x86-64
+
+Sections:
+Idx Name          Size      VMA               LMA               File off  Algn
+  0 .text         0000002c  0000000000000000  0000000000000000  00000228  2**4
+                  CONTENTS, ALLOC, LOAD, CODE
+  1 .data         00000004  000000000000002c  000000000000002c  00000254  2**2
+                  CONTENTS, ALLOC, LOAD, DATA
+  2 __LD.__compact_unwind 00000020  0000000000000030  0000000000000030  00000258  2**3
+                  CONTENTS, RELOC, DEBUGGING
+  3 .eh_frame     00000040  0000000000000050  0000000000000050  00000278  2**3
+                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+ */
+
+//再来dump 生成的ab  objdump -h ab
+/*
+ab:     file format mach-o-x86-64
+
+Sections:
+Idx Name          Size      VMA               LMA               File off  Algn
+  0 .text         0000005c  0000000000001f70  0000000000001f70  00000f70  2**4
+                  CONTENTS, ALLOC, LOAD, CODE
+  1 .eh_frame     00000030  0000000000001fd0  0000000000001fd0  00000fd0  2**3
+                  CONTENTS, ALLOC, LOAD, READONLY, DATA
+  2 .data         00000004  0000000000002000  0000000000002000  00001000  2**2
+                  CONTENTS, ALLOC, LOAD, DATA
+ */
