0476ab7cdb3749192847882bf8fa67fe9ecdbbcf
DuckDeck <3421902@qq.com>
2020-08-14 17:36:37 +0800

    remove js

--- Data & Algorithm/Data Stucture/search/arraySearchTest.js
+++
@@ -1,6 +0,0 @@
-require('./array.extensions')
-let t = [1,3,4,66,12,2,33,123,41]
-console.log(t.sequentialSearch(4))
-t.quickSort()
-console.log(t)
-console.log(t.fibonacciSearch(3)) //二分查找一定要用有顺序的数组
